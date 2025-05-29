7e13ec74706a5c1cca3dba5f3c57e6e84124109b
REBOSOLUTIONS <siva.prasad@rebosolution.com>
2025-05-29 14:30:07 +0530

    Recommit without secrets 2

--- server/database.py
+++ server/database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine, Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_LOCAL_URL = "postgresql://postgres:1q2w3e4r@localhost:5432/crypto"
-# DATABASE_REMOTE_URL = "postgresql://xfx2025:OAg0tocVgxBkg98JDWA4EUp60M4D48r4@dpg-d0j2pladbo4c73bvhjhg-a.oregon-postgres.render.com/crypto_l5ku"
+# DATABASE_LOCAL_URL = "postgresql://postgres:1q2w3e4r@localhost:5432/crypto"
+DATABASE_REMOTE_URL = "postgresql://xfx2025:OAg0tocVgxBkg98JDWA4EUp60M4D48r4@dpg-d0j2pladbo4c73bvhjhg-a.oregon-postgres.render.com/crypto_l5ku"
 
-engine = create_engine(DATABASE_LOCAL_URL)
-# engine = create_engine(DATABASE_REMOTE_URL)
+# engine = create_engine(DATABASE_LOCAL_URL)
+engine = create_engine(DATABASE_REMOTE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
